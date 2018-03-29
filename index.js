6a31a3f9d00e49873fd385f35e7e8067921a715f
btcline04 <btcline04@gmail.com>
2018-03-29 10:32:57 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget(target) {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = ranked.length; i < l; i++) {
+    let children = ranked[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  let nodes = document.querySelectorAll('#grand-node div');
+  let deepNode = nodes[nodes.length - 1];
+  return deepNode;
+}
